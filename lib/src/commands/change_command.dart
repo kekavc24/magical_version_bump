d68da2de57e4a70d10b6981fd513f07c183a8239
Kavisi <68240897+kekavc24@users.noreply.github.com>
2023-05-14 17:53:12 +0100

    refractor: removed value help from options

--- lib/src/commands/change_command.dart
+++ lib/src/commands/change_command.dart
@@ -19,38 +19,31 @@ class ChangeNodesCommand extends Command<int> {
       ..addMultiOption(
         'name',
         help: '''Change the name in pubspec.yaml''',
-        valueHelp: 'Hello Word',
       )
       ..addOption(
         'description',
         help: '''Change the description of your project in pubspec.yaml''',
-        valueHelp: 'This project says hello to the world',
       )
       ..addOption(
         'yaml-version',
         help: '''Option to completely change the version in pubspec.yaml''',
-        valueHelp: '1.1.0',
       )
       ..addOption(
         'homepage',
         help: '''Change the homepage url in pubspec.yaml''',
-        valueHelp: 'https://url.to.homepage',
       )
       ..addOption(
         'repository',
         help: '''Change the repository url for project in pubspec.yaml''',
-        valueHelp: 'https://url.to.repository-on-github',
       )
       ..addOption(
         'issue_tracker',
         help: '''Change the url pointing to an issue tracker in pubspec.yaml''',
-        valueHelp: 'https://url.to.issue-tracker',
       )
       ..addOption(
         'documentation',
         help:
             '''Change the url pointing to your project's documentation in pubspec.yaml''',
-        valueHelp: 'https://url.to.documentation',
       );
   }
 
