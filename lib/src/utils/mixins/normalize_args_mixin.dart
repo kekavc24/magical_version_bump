6a889848ae634851407ccb147c9080f7671dd14a
Kavisi <68240897+kekavc24@users.noreply.github.com>
2023-05-27 20:24:21 +0100

    feat: add check for whether user set path

--- lib/src/utils/mixins/normalize_args_mixin.dart
+++ lib/src/utils/mixins/normalize_args_mixin.dart
@@ -2,24 +2,36 @@ typedef ArgsAndValues = Map<String, String>;
 
 /// This mixin normalizes arguments passed passed in by user
 mixin NormalizeArgs {
-  /// Normalize arguments. Remove '-' or '--' present
-  List<String> normalizeArgs(List<String> args) => args.map((e) {
-        var mod = e.replaceFirst(RegExp('--'), '');
+  /// Normalize arguments. Remove '-' or '--' present.
+  ///
+  /// Also obtains the set path
+  ({List<String> args, bool hasPath, String? setPath}) normalizeArgs(
+    List<String> args,
+  ) {
+    final sanitizedArgs = args.map((e) {
+      var mod = e.replaceFirst(RegExp('--'), '');
 
-        if (mod[0] == '-') mod = mod.replaceFirst(RegExp('-'), '');
+      if (mod[0] == '-') mod = mod.replaceFirst(RegExp('-'), '');
 
-        return mod;
-      }).toList();
+      return mod;
+    }).toList();
+
+    return (
+      args: sanitizedArgs.where((arg) => !arg.contains('set-path')).toList(),
+      hasPath: sanitizedArgs.contains('set-path'),
+      setPath: sanitizedArgs.contains('set-path')
+          ? sanitizedArgs.firstWhere((arg) => arg.contains('set-path'))
+          : null
+    );
+  }
 
   /// Prep normalized args
   ({
     bool absoluteVersioning,
     String action,
     List<String> versionTargets,
-    bool requestPath
-  }) prepArgs(
-    List<String> args,
-  ) {
+    bool requestPath,
+  }) prepArgs(List<String> args) {
     final actionFlag = args.first; // Action command
 
     // Targets
