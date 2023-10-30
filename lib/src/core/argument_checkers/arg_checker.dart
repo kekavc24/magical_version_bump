ba398094f75f73d7e4cc94651947ce822dae594b
Kelvin Kavisi <68240897+kekavc24@users.noreply.github.com>
2023-10-30 03:00:44 +0000

    refactor: clean-up arg checker class > make base class abstract > add custom overrides for subtypes > update test to to reflect changes

--- lib/src/core/argument_checkers/arg_checker.dart
+++ lib/src/core/argument_checkers/arg_checker.dart
@@ -11,15 +11,15 @@ part 'set_args_checker.dart';
 /// Contains basic code implementations to
 ///   * Prep args to desired format for each command
 ///   * Validate arguments
-base class ArgumentsChecker {
+abstract class ArgumentsChecker {
   ArgumentsChecker({required this.argResults});
 
   /// Argument results from command
   final ArgResults? argResults;
 
   /// Basic implementation to check if args are empty or null
-  ({bool isValid, InvalidReason? reason}) defaultValidation() {
-    // Args must not be empty or null
+  ({bool isValid, InvalidReason? reason}) validateArgs() {
+    /// Args must not be empty or null. 
     if (argResults == null || argResults!.arguments.isEmpty) {
       return (
         isValid: false,
@@ -34,8 +34,5 @@ base class ArgumentsChecker {
   }
 
   /// Prep args to desired format
-  void prepArgs() {}
-
-  /// Validate arguments
-  void validateArgs() {}
+  void prepArgs();
 }
