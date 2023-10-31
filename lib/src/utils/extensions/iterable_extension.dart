532bb6eef3fee2ac000ab846cb16cd4938421470
Kelvin Kavisi <68240897+kekavc24@users.noreply.github.com>
2023-10-31 22:54:29 +0000

    refactor: add extended json/yaml support #42 > allow for adding of maps in list > allow for adding string/lists to map. map will be converted to list > add support for recursive update of keys within lists > add support for updates within lists nested in lists

--- lib/src/utils/extensions/iterable_extension.dart
+++ lib/src/utils/extensions/iterable_extension.dart
@@ -1,3 +1,6 @@
+import 'package:magical_version_bump/src/utils/enums/enums.dart';
+import 'package:magical_version_bump/src/utils/extensions/string_extensions.dart';
+
 extension Operations on Iterable<String> {
   /// Get target that will be used to update whole version relative to its
   /// position. Affinity/preference in descending order is:
@@ -41,5 +44,64 @@ extension Operations on Iterable<String> {
         (element) => element.isNotEmpty,
       );
     return retainedList;
+  }
+
+  /// Check how many values match a condition of search string
+  MatchCount checkMatchCount(String pattern) {
+    if (every((element) => element.contains(pattern))) {
+      return MatchCount.all;
+    }
+
+    if (any((element) => element.contains(pattern))) {
+      return MatchCount.some;
+    }
+
+    return MatchCount.none;
+  }
+
+  /// Output list of values for dictionary based on match count
+  dynamic splitBasedOnMatch() {
+    const pattern = '->';
+
+    final matchCount = checkMatchCount(pattern);
+
+    // If all are maps, return an enclosed map of values
+    if (matchCount == MatchCount.all) {
+      return fold(
+        <String, String>{},
+        (previousValue, element) {
+          final map = element.splitAndTrim(pattern);
+          previousValue.update(
+            map.first,
+            (value) => map.last.isEmpty ? 'null' : map.last,
+            ifAbsent: () => map.last.isEmpty ? 'null' : map.last,
+          );
+          return previousValue;
+        },
+      );
+    }
+
+    // If mixed with some strings, split individually
+    if (matchCount == MatchCount.some) {
+      return fold(
+        <dynamic>[],
+        (previousValue, element) {
+          // Extract map and add it
+          if (element.contains(pattern)) {
+            final map = element.splitAndTrim(pattern);
+            previousValue.add(
+              {map.first: map.last.isEmpty ? 'null' : map.last},
+            );
+          } else {
+            previousValue.add(element);
+          }
+
+          return previousValue;
+        },
+      );
+    }
+
+    // Just return "as-is" if none
+    return this;
   }
 }
